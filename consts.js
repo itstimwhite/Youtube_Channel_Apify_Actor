939c7f79828c208fa3d3af67d283505d1778c16e
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-01-18 14:19:42 -0500

    Add files via upload

---
+++ consts.js
@@ -0,0 +1,3 @@
+exports.SELECTORS = {
+    channelName: '/html/body/ytd-app/div/ytd-page-manager/ytd-browse/div[3]/ytd-c4-tabbed-header-renderer/tp-yt-app-header-layout/div/tp-yt-app-header/div[2]/div[2]/div/div[1]/div/div[1]/ytd-channel-name/div/div/yt-formatted-string'
+}
